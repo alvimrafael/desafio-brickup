8b1879bd438f4da324fe6d7fa10b419b7c469ae0
Rafael Alvim <rafael.alvimdossantos@gmail.com>
2023-12-19 20:36:39 -0300

    inicializando o repositório

---
+++ src/main/java/com/brickupdesafio/desafiotarefas/DesafioTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.brickupdesafio.desafiotarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brickupdesafio/desafiotarefas/DesafioTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brickupdesafio.desafiotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
